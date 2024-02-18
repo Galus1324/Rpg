fec6b2746d342c53b8f59eb7652a8ad0fd598be7
Galus1324 <gal.pestotnik@gmail.com>
2024-02-18 15:51:13 +0100

    fixed some stuff

--- zibiti.py
+++ zibiti.py
@@ -55,14 +55,34 @@ def print_slow(args):
 # Function for multiple choice interactions
 def multi_choice(answers, consequences):
     print_slow("Choose an option:\n")
+    print_slow(f"Available answers: {', '.join(answers)}\n")
     x = input().lower()
 
     if x not in answers:
-        print_slow(f"Your answer '{x}' is not valid.\nValid answers are: {', '.join(answers)}\n")
+        print_slow(f"Your answer '{x}' is not valid.\n")
         multi_choice(answers, consequences)
     else:
         index = answers.index(x)
         consequences[index]()
+
+# for looping choices
+def circle_choice(answers, consequences):
+    while answers:
+        print_slow("Choose an option:\n")
+        print_slow(f"Available answers: {', '.join(answers)}\n")
+        x = input().lower()
+
+        if x not in answers:
+            print(f"Your answer '{x}' is not valid.\nValid answers are: {', '.join(answers)}\n")
+        else:
+            index = answers.index(x)
+            consequences[index]()
+            del answers[index]
+            del consequences[index]
+
+
+
+
 
 # Example usage:
 filename = "text_for_rpg.txt"
@@ -86,21 +106,22 @@ def soba3():
 # Dynamically create variables from extracted sections
 locals().update(extracted_sections)
 
+
 print_slow(name)
 player_name = input()
 print_slow(intro)
 
 multi_choice(["beer", "mead", "nothing"],
-             [lambda beer=locals().get("beer", ""): print_slow(beer),
-              lambda mead=locals().get("mead", ""): print_slow(mead),
-              lambda nothing=locals().get("nothing", ""): (print_slow(nothing),
-                                                           globals().update({"drink": False}))])
+             [lambda: print_slow(beer),
+              lambda: print_slow(mead),
+              lambda: (print_slow(nothing),
+                       globals().update({"drink": False}))])
 
 print_slow(bartender_talk)
 
 multi_choice(["yes", "no"],
-             [lambda toa_yes=locals().get("toa_yes", ""): print_slow(toa_yes),
-              lambda toa_no=locals().get("toa_no", ""): print_slow(toa_no)])
+             [lambda: print_slow(toa_yes),
+              lambda: print_slow(toa_no)])
 
 if drink:
     print_slow(drink_yes)
@@ -110,8 +131,8 @@ else:
 print_slow(outside_village.format(player_name=player_name))
 
 multi_choice(["yes", "no"],
-             [lambda help_eirlys=locals().get("help_eirlys", ""): print_slow(help_eirlys.format(player_name=player_name)),
-              lambda no_help_eirlys=locals().get("no_help_eirlys", ""): (print_slow(no_help_eirlys), globals().update({'toa_map': False})),
+             [lambda: print_slow(help_eirlys.format(player_name=player_name)),
+              lambda: (print_slow(no_help_eirlys), globals().update({'toa_map': False})),
               lambda: None])
 
 
@@ -120,7 +141,13 @@ if toa_map:
     multi_choice(["yes", "no"],[lambda: journey_start(), lambda: None])
 
 def blacksmith():
-    print("blacksmith")
+    print_slow(go_to_blacksmith)
+    circle_choice(["upgrade gear", "talk","go away"],
+                 [lambda: print_slow(upgrade_1),
+                  lambda: print_slow(blacksmith_talk_1),
+                  lambda: print_slow(blacksmith_go_away)])
+
+    
 def alchemist():
     print("alchemist")
 def merchaint():
@@ -130,11 +157,13 @@ def tavern_2():
 def wonder():
     print("wonder")
 
+blacksmith()
 
 # If they stay in the village
 print_slow(stay_in_the_village)
-multi_choice([["blacksmith"],["alchemist"],["merchaint"],["tavern"],["wonder"]],
-             [lambda: blacksmith(), alchemist(), merchaint(), tavern_2(), wonder()]) 
+circle_choice(["blacksmith","alchemist","merchaint","tavern","wonder", "go towards the tomb"],
+              [lambda: blacksmith(), lambda: alchemist(), lambda: merchaint(), lambda: tavern_2(), lambda: wonder(), lambda: journey_start()]) 
+
 
 
 
